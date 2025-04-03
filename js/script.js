121ae79aaf8b541100239efe6cfdfa440902cc68
Carlomadella <madella871@gmail.com>
2025-04-03 16:44:53 +0200

    [UPD] creato ciclo for

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@
 const list =  document.getElementById("lista")
 // creato array contenente le email
 const emailArray = [];
+// creato ciclo for per chiedere all'API un'email generata randomicamente 
+for (let i = 0; i < 10; i++) {
+    
+}
 
 
 
