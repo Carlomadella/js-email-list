5c0e1356e88e283b7476f2cc5cacffa2351e5758
Carlomadella <madella871@gmail.com>
2025-04-03 16:44:13 +0200

    [UPD] recuperata lista dal dom e creato array che dovrà contenere le email

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+
+//  recupero il div nell'HTML con id lista e lo salvo nella variabile list
+const list =  document.getElementById("lista")
+// creato array contenente le email
+const emailArray = [];
+
+
+
+
+
+
+
